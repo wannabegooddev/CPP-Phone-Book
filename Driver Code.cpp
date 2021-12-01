f94b0b55cbb5e65080eba17e28c32da6e349a66e
Giga Hidjrika <52191436+Infinicus@users.noreply.github.com>
2021-12-01 10:18:34 +0700

    Update Driver Code.cpp

--- Driver Code.cpp
+++ Driver Code.cpp
@@ -11,7 +11,32 @@ int main()
 
     while(!end_program)
     {
-
+        cout << "=====================================" << endl;
+        cout << "Selamat datang di program buku telpon. Apa yang mau Anda lakukan?\n";
+        cout << "1. Menampilkan seluruh kontak\n2. Mengubah info kontak\n3. Menambah kontak\n4. Menghapus kontak\n5. Mencari kontak\n6. Keluar\n";
+        cout << "=====================================" << endl;
+        cin >> choice;
+        switch(choice)
+        {
+        case 1:
+            cout << "Menampilkan seluruh kontak\n";
+            break;
+        case 2:
+            cout << "Mengubah info kontak\n";
+            break;
+        case 3:
+            cout << "Menambah kontak\n";
+            break;
+        case 4:
+            cout << "Menghapus kontak\n";
+            break;
+        case 5:
+            cout << "Mencari kontak\n";
+            break;
+        case 6:
+            exit(0);
+            break;
+        }
     }
 
     return 0;
