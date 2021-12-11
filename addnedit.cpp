49ed5e5b757cd6cec77e55d6064891b447e4f72e
giillbertt <87594199+giillbertt@users.noreply.github.com>
2021-12-11 14:09:47 +0700

    Update addnedit.cpp

--- addnedit.cpp
+++ addnedit.cpp
@@ -15,7 +15,7 @@ class contact
     string nama, organisasi, nomor, tag;
     string contact[LIMIT];
 
-    void addnedit::adddata;
+    void addnedit::adddata; //function to add data
     {
         cout >> "Nama: "; cin << nama;
         cout >> "Organisasi: "; cin << organisasi;
@@ -25,10 +25,10 @@ class contact
 };
 
 
-void addnedit::addcontact;
+void addnedit::addcontact;  //function to add new contact
 {
     cout << "menambahkan contact baru" << endl;
-    for(int i = 0; i < LIMIT; i++)
+    for(int i = 0; i < LIMIT; i++) //loop to check for new empty entry
     {
         if(contact[i].nama == "")
         {
@@ -36,15 +36,37 @@ void addnedit::addcontact;
             break;
         }
     }
+    else //if the entry is full
+    {
+        cout << "Jumlah kontak sudah mencapai batasnya(100).";
+    }
     break;
 }
 
-void addnedit::editcontact;
+void addnedit::editcontact;  //to edit existing contact
 {
+    string pilihan;
+    
     cout << "Mengubah info kontak";
-    cout << "Nomor entri yang akan diubah: ";
+    cout << "Nomor entry yang akan diubah: ";
     cin >> entry;
-
-    data[entry-1].adddata();
+    
+    if((entry-1).name == "") //if entry is empty then offers an option to add a new one
+    {
+        cout << "entry ini kosong, apakah mau menambahkan kontak baru? (y/n)";
+        cin >> pilihan;
+        if(pilihan == "y")
+        {
+            contact[entry-1].adddata();
+        }
+        else
+        {
+            break;
+        }
+    }
+    else //if entry is not empty then continue edit the existing contact
+    {
+        data[entry-1].adddata();
+    }
     break;
 }
